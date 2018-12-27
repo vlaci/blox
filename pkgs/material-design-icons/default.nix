ea7bc33058636e62fa26ce974c49bf1365f16eef
László Vaskó <laszlo.vasko@outlook.com>
2018-12-27 23:18:33 +0100

    pkgs/material-design-icons: updating to '3.2.89'

--- pkgs/material-design-icons/default.nix
+++ pkgs/material-design-icons/default.nix
@@ -2,18 +2,21 @@
 
 stdenv.mkDerivation rec {
   name = "material-design-icons-${version}";
-  version = "2.1.19";
-  cmakeFlags = "-DOVERRIDE_VERSION=${version}";
+  version = "3.2.89";
 
   src = fetchFromGitHub {
     owner  = "Templarian";
     repo   = "MaterialDesign-Webfont";
     rev    = "v${version}";
-    sha256 = "0jyjb9a44c9lm45x07gba75badfaas7cgdfhv8am7vwy88fsxhjz";
+    sha256 = "1rxaiiij96kqncsrlkyp109m36v28cgxild7z04k4jh79fvmhjvn";
   };
 
   buildCommand = ''
-    mkdir -p $out/share/fonts/truetype
-    install -D -m644 "$src/fonts/materialdesignicons-webfont.ttf" "$out/share/fonts/truetype/MaterialDesignIcons.ttf"
+    mkdir -p $out/share/fonts/{eot,svg,truetype,woff,woff2}
+    cp $src/fonts/*.eot $out/share/fonts/eot/
+    cp $src/fonts/*.svg $out/share/fonts/svg/
+    cp $src/fonts/*.ttf $out/share/fonts/truetype/
+    cp $src/fonts/*.woff $out/share/fonts/woff/
+    cp $src/fonts/*.woff2 $out/share/fonts/woff2/
   '';
 }
