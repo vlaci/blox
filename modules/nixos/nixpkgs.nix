1669c10dc708249691cb3eb78e90c523e402b866
László Vaskó <vlaci@balabit.hu>
2019-11-24 16:54:29 +0100

    Revert "emacs: use nix-community overlay"
    This reverts commit b0481cc68826a06aed487a58349e245f596cfe4a.

--- modules/nixos/nixpkgs.nix
+++ modules/nixos/nixpkgs.nix
@@ -12,16 +12,13 @@
       pkgs = super;
       bloxpkgs = import ../../pkgs { inherit pkgs; };
       mozilla = import <mozilla/package-set.nix> { inherit pkgs; };
-      emacs = import (builtins.fetchTarball {
-        url = https://github.com/nix-community/emacs-overlay/archive/master.tar.gz;
-      }) self super;
       unstable = import <nixos-unstable> {
         config = config.nixpkgs.config;
         # do not apply overlays recursively as it would introduce infinite recursion
         overlays = [];
       };
     in {
-      bloxpkgs = bloxpkgs // { inherit mozilla unstable emacs; };
+      bloxpkgs = bloxpkgs // { inherit mozilla unstable; };
     })
   ];
 }
