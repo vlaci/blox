498503a3b98a0cd277d7de1cc774cd1079add2c7
László Vaskó <vlaci@balabit.hu>
2020-03-01 19:28:12 +0100

    bloxpkgs.phpPackages.composer2nix: workaround php 7.3 incompatibility

--- pkgs/phpPackages/default.nix
+++ pkgs/phpPackages/default.nix
@@ -7,5 +7,5 @@
     repo = "composer2nix";
     rev = "v0.0.3";
     sha256 = "1xa4qrknzz74fxqqihh7san56sq2wiy39n282zrid8zm4y2yl4s6";
-    }) { inherit pkgs system; noDev = true; }).override { executable = true; };
+  }) { inherit system; pkgs = (pkgs.extend (se: su: { php = se.php72; })); noDev = true; }).override { executable = true; };
 }
