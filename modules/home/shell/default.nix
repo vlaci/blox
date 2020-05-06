6662f2f2f77ec9570aaf262c8d9689eb3f6714a3
László Vaskó <laszlo.vasko@outlook.com>
2020-05-06 08:51:34 +0200

    squash! profiles/shell: reworked prompt (powerlevel10k wip)

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -23,9 +23,17 @@ in {
         # Correct spelling of all arguments in the command line
         setopt CORRECT_ALL
 
+        # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+        # Initialization code that may require console input (password prompts, [y/n]
+        # confirmations, etc.) must go above this block; everything else may go below.
+        if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+          source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+        fi
 
         source $ZDOTDIR/grml-zshrc
         source $ZDOTDIR/zshrc.local
+        source ${pkgs.bloxpkgs.unstable.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+        [[ ! -f $ZDOTDIR/.p10k.zsh ]] || source $ZDOTDIR/.p10k.zsh
       '';
       plugins = [
         {
@@ -52,7 +60,6 @@ in {
       bashInteractive  # to ensure bash with readline sujpport even if it isn't the default shell
     ];
 
-    programs.starship.enable = true;
     programs.fzf.enable = true;
     services.lorri.enable = true;
   };
