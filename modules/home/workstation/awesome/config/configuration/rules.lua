6599e14f7bf929bf5137745780965d9af560fb6f
László Vaskó <laszlo.vasko@outlook.com>
2020-09-11 21:54:20 +0200

    modules/workstation: cleanup awesome rules

--- modules/home/workstation/awesome/config/configuration/rules.lua
+++ modules/home/workstation/awesome/config/configuration/rules.lua
@@ -23,25 +23,16 @@ local rules = {
     -- Floating clients.
     { rule_any = {
         instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
         },
         class = {
           "Arandr",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Wpa_gui",
           "pinentry",
-          "veromix",
-          "xtightvncviewer"},
-
+          "xtightvncviewer"
+        },
         name = {
           "Event Tester",  -- xev.
         },
         role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
           "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
         }
       }, properties = { floating = true }},
