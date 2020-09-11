1230f8ebf68ef4fcce951d587c71520b744b61ac
László Vaskó <laszlo.vasko@outlook.com>
2020-09-11 21:58:06 +0200

    modules/workstation: lua 5.3 support in awesome

--- modules/home/workstation/awesome/config/widgets/lib.lua
+++ modules/home/workstation/awesome/config/widgets/lib.lua
@@ -4,12 +4,11 @@ local color = require("gears.color")
 local function get_color_for_progress(fact)
     local u_r, u_g, u_b, u_a = color.parse_color(beautiful.fg_urgent)
     local n_r, n_g, n_b, n_a = color.parse_color(beautiful.fg_normal)
-
     return string.format(
         "#%02x%02x%02x",
-        255 * (u_r + fact * (n_r - u_r)),
-        255 * (u_g + fact * (n_g - u_g)),
-        255 * (u_b + fact * (n_b - u_b))
+        math.floor(255 * (u_r + fact * (n_r - u_r))),
+        math.floor(255 * (u_g + fact * (n_g - u_g))),
+        math.floor(255 * (u_b + fact * (n_b - u_b)))
     )
 end
 
